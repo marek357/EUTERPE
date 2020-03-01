10cd55ded096261ed55a53edf7168be9269a14d2
marek357 <mm.masiak@student.uw.edu.pl>
2020-03-01 23:04:03 +0100

    test

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# import numpy as np
-import pandas as pd
-from pprint import pprint
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import Dropout
-# from keras.layers import LSTM
-# from keras.callbacks import ModelCheckpoint
-# from keras.utils import np_utils
 from src.dataHelper import DataHelper
 
+
 if __name__ == '__main__':
-    lista_utworow = DataHelper.load_lyrics('Country')
+    lista_utworow, char_to_int = DataHelper.load_lyrics('Country')
--- src/dataHelper.py
+++ src/dataHelper.py
@@ -1,3 +1,5 @@
+from typing import Tuple, List, Dict
+
 import pandas as pd
 
 
@@ -10,21 +12,29 @@ class DataHelper:
         pass
 
     @staticmethod
-    def load_lyrics(genre):
+    def load_lyrics(genre: str) -> Tuple[List[str], Dict[set, int]]:
         """
         Funkcja laduje z dolaczonego pliku csv odpowiednie teksty
-        zgodne z gatunkiem.
+        zgodne z gatunkiem oraz zwraca slownik przekonwertowanych
+        znakow na liczby.
 
         Parameters:
             genre (string): Gatunek muzyki.
 
         Returns:
             lyrics_list (list): Lista tekstow utworow.
+            char_to_int (dict of char: int): Slownik znakow
+            i odpowiadajacych im liczb
         """
 
-        lyrics_df = pd.read_csv('../res/lyrics.csv')
+        lyrics_df = pd.read_csv('res/lyrics.csv')
         lyrics_list = []
+        char_to_int = {}
+        char_set = set()
         for text, genre_value in zip(lyrics_df['lyrics'], lyrics_df['genre']):
             if genre_value == genre:
                 lyrics_list.append(str(text).lower())
-        return lyrics_list 
+                char_set.update(set(str(text)))
+        for index, elem in enumerate(sorted(list(char_set))):
+            char_to_int[elem] = index
+        return lyrics_list, char_to_int
