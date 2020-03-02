3db94b7a5fe8ac8557a2a0bf59c7f8ea32047eaa
marek357 <mm.masiak@student.uw.edu.pl>
2020-03-02 19:34:32 +0100

    funkcja encodujaca

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# import numpy as np
-import pandas as pd
-from pprint import pprint
-# from keras.models import Sequential
-# from keras.layers import Dense
-# from keras.layers import Dropout
-# from keras.layers import LSTM
-# from keras.callbacks import ModelCheckpoint
-# from keras.utils import np_utils
 from src.dataHelper import DataHelper
+from pprint import pprint
 
 if __name__ == '__main__':
-    lista_utworow = DataHelper.load_lyrics('Country')
+    lista_utworow, char_to_int = DataHelper.load_lyrics('Country')
+    x, y = DataHelper.encode_data(lista_utworow, char_to_int)
+    pprint(x)
+    pprint(y)
--- src/dataHelper.py
+++ src/dataHelper.py
@@ -1,3 +1,5 @@
+from typing import Tuple, List, Dict
+import re
 import pandas as pd
 
 
@@ -10,21 +12,79 @@ class DataHelper:
         pass
 
     @staticmethod
-    def load_lyrics(genre):
+    def load_lyrics(genre: str) -> Tuple[List[str], Dict[str, int]]:
         """
         Funkcja laduje z dolaczonego pliku csv odpowiednie teksty
-        zgodne z gatunkiem.
+        zgodne z gatunkiem oraz zwraca slownik przekonwertowanych
+        znakow na liczby. Z tekstow usuwane sa wszelkie znaki nie
+        bedace alfanumerycznymi, albo diakrytycznymi
 
         Parameters:
             genre (string): Gatunek muzyki.
 
         Returns:
             lyrics_list (list): Lista tekstow utworow.
+            char_to_int (dict of char: int): Slownik znakow
+            i odpowiadajacych im liczb
         """
 
-        lyrics_df = pd.read_csv('../res/lyrics.csv')
+        lyrics_df = pd.read_csv('res/lyrics.csv')
         lyrics_list = []
+        char_to_int = {}
+        dlugosc = 0
+        char_set = set()
         for text, genre_value in zip(lyrics_df['lyrics'], lyrics_df['genre']):
             if genre_value == genre:
-                lyrics_list.append(str(text).lower())
-        return lyrics_list 
+                lyrics_list.append(
+                    re.sub(
+                        r"[^0-9a-z.,' ]+",
+                        '',
+                        str(text).lower()
+                    )
+                )
+                dlugosc += len(lyrics_list[-1])
+                char_set.update(set(str(text)))
+        for index, elem in enumerate(sorted(list(char_set))):
+            char_to_int[elem] = index
+        return lyrics_list, char_to_int
+
+    @staticmethod
+    def encode_data(lyrics_list: List[str], char_to_int: Dict[str, int]
+                    ) -> Tuple[List[List[int]], List[int]]:
+        """
+        Funkcja iteruje sie po tekscie kazdej piosenki i tworzy
+        tablice X,y, posiadające wzorce do feedowania do LSTMu
+
+        TODO zmienic - na razie mamy ciagi ustalonej dlugosci
+        (jak w tutorialu), ale możemy zrobic tokenizacje i
+        zdaniami feedowac. Nie mam pojecia czy to cokolwiek
+        ulepszy, ale mozemy sprobowac.
+
+        Parameters:
+            lyrics_list (list): Lista tekstow utworow.
+            char_to_int (dict of char: int): Slownik znakow
+            i odpowiadajacych im liczb
+
+        Returns:
+            X (list of list): Ciagi znakow do ktorych y jest uzupelnieniem.
+            y (list): Przewidywania.
+        """
+
+        dlugosc_wzorca = 100
+        x_list = []
+        y_list = []
+        continuous_lyric = ' '.join(lyrics_list)
+        for i in range(len(continuous_lyric) - dlugosc_wzorca):
+            y_list.append(char_to_int[continuous_lyric[i + dlugosc_wzorca]])
+            temp_x = []
+            for elem in continuous_lyric[i: i + dlugosc_wzorca]:
+                temp_x.append(char_to_int[elem])
+            x_list.append(temp_x)
+        return x_list, y_list
+
+    @staticmethod
+    def save_data(x_list: List[List[int]], y_list: List[int], path: str) -> None:
+        with open(path + 'x_list.json') as file:
+            file.write(str(x_list))
+        with open(path + 'y_list.json') as file:
+            file.write(str(y_list))
