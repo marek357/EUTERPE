d4de070a2cc425619c402482cbe4fee2bd686b28
marek357 <marekmmasiak@gmail.com>
2020-03-06 17:40:56 +0100

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:2
-#SBATCH --n=5
-#SBATCH -c=1
+#SBATCH -n 5
+#SBATCH -c 1
 #SBATCH --account=GS79-10
 #SBATCH -J NLP
 #SBATCH -o EUTERPE_run%j.out
