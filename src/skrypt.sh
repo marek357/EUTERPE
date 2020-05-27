9c2c3a5199f50950579933a7270973f6956b938b
marek357 <marekmmasiak@gmail.com>
2020-05-28 00:43:00 +0200

    Update skrypt.sh

--- src/skrypt.sh
+++ src/skrypt.sh
@@ -2,6 +2,22 @@
 
 #skrypt można uruchomić po wygenerowaniu muzyki za pomocą skryptu muzyka.py oraz tekstu za pomocą skryptu tekst.py
 
+rm generowanie_muzyki/muzyka.mid generowanie_muzyki/wokal.mid
+
+cd generowanie_muzyki
+
+python3 muzyka.py <<< "N"
+
+cd ..
+
+echo "Wygenerowano muzykę"
+
+cd generowanie_tekstu
+
+python3 tekst.py < ../infoTekst.txt
+
+cd ..
+
 python3 generowanie_muzyki/midi2voice.py generowanie_tekstu/xd.txt generowanie_muzyki/wokal.mid female "$(< generowanie_muzyki/wokalTekst.txt)"
 
 timidity generowanie_muzyki/muzyka.mid -Ow -o muzyka.wav
